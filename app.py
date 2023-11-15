6cc978e18a2b2ed25418b0de8c0da42f2d74990a
Saahrulrmdhnn <144138542+Saahrulrmdhnn@users.noreply.github.com>
2023-11-15 13:20:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,42 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request
+import requests
+import bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("mongodb+srv://sahrul:sahrul@cluster0.nsaxt60.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME =  os.environ.get("DB_NAME")
-
-client = MongoClient(MONGODB_URI)
-db = client[DB_NAME]
+conenction_string = 'mongodb+srv://sahrul:sahrul@cluster0.nsaxt60.mongodb.net/?retryWrites=true&w=majority'
+db = MongoClient(conenction_string)
+db = client.dbsparta
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    # sample_receive = request.form['sample_give']
-    bucket_receive = request.form['bucket_give']
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({}, {'_id' : False}))
+    return jsonify({'articles' : articles})
 
-    count = db.bucket.count_documents({})
-    num = count + 1
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    #sample_receive = request.form['sample_give']
+    #print(sample_receive)
+    title_receive = request.from.get('title_give')
+    content_receive = request.from.get('content_give')
+    
+    file = request.files["file_give"]
+    save_to = 'static/mypicture.jpg'
+    file.save(save_to)
 
     doc = {
-        'num': num,
-        'bucket': bucket_receive,
-        'done': 0,
+        'title' : filename,
+        'title': title_receive
+        'content':content_receive
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': 'data saved!'})
-
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one(
-        {'num': int(num_receive)},
-        {'$set': {'done': 1}}
-    )
-    return jsonify({'msg': 'update done!'})
-
-@app.route("/delete", methods=["POST"])
-def delete_bucket():
-    num_receive = request.form['num_give']
-    db.bucket.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': 'delete done!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': buckets_list})
+    db.diary.insert_one(doc)
+    return jsonify({'message':'data was!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
